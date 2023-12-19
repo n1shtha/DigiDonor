46db76d6ed5957b0d8d1fc0184ab4175e2c8ccca
chanchal-bajoria <chanchal.bajoria_ug23@ashoka.edu.in>
2023-12-20 00:18:12 +0530

    exported backend/javascript/registerUser.js into backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,41 @@
 const express = require('express');
+const { Gateway, Wallets } = require('fabric-network');
 const path = require('path');
-const bodyParser = require('body-parser');
-const { Gateway, Wallets } = require("fabric-network");
-const FabricCAServices = require("fabric-ca-client");
-const fs = require("fs");
+const fs = require('fs');
+
+const registerUser = require('./javascript/registerUser');
 
 const app = express();
+const PORT = 3001;
+
 app.use(bodyParser.json());
 
-const ccpPath = path.resolve(__dirname, '..', 'connection.json');
-const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
-const ccp = JSON.parse(ccpJSON);
+// const ccpPath = path.resolve(__dirname, '..', 'connection.json');
+// const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+// const ccp = JSON.parse(ccpJSON);
 
-const gateway = new Gateway();
-await gateway.connect(ccp, {
-    wallet: await Wallets.newFileSystemWallet('./wallet'),
-    identity: 'user1',
-    discovery: { enabled: true, asLocalhost: true }
-});
+// const gateway = new Gateway();
+// await gateway.connect(ccp, {
+//     wallet: await Wallets.newFileSystemWallet('./wallet'),
+//     identity: 'user1',
+//     discovery: { enabled: true, asLocalhost: true }
+// });
 
-const network = await gateway.getNetwork('mychannel');
+// const network = await gateway.getNetwork('mychannel');
+
+app.use(express.json());
 
 app.post('/signup', async (req, res) => {
     const { username, password, userType } = req.body;
     try {
-        await contract.submitTransaction('registerUser', username, password, userType);
+        await registerUser(username, password, userType);
+        //await contract.submitTransaction('registerUser', username, password, userType);
         res.json({ success: true });
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ success: false, error: error.message });
     }
 });
 
-app.listen(8080, () => {
-    console.log('Server running on http://localhost:8080');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
