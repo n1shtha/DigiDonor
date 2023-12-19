7002741f471b1ddffef83f4394b0f59d9520167b
chanchal-bajoria <chanchal.bajoria_ug23@ashoka.edu.in>
2023-12-20 00:35:54 +0530

    minor import errors fixed

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
-import "./styles.css";
+import "../styles.css";
 
 function Signup() {
 
