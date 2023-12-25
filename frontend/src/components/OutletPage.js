302da0b5afe410c5fc80e33ecbb7355d84506b5f
chanchal-bajoria <chanchal.bajoria_ug23@ashoka.edu.in>
2023-12-26 01:51:27 +0530

    redeem bug fixes, success page

--- frontend/src/components/OutletPage.js
+++ frontend/src/components/OutletPage.js
@@ -1,9 +1,52 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, {useState}  from "react";
+import axios from "axios";
+import { useParams, useNavigate} from "react-router-dom";
 import "../styles.css";
 
 function OutletPage() {
   const { outlet } = useParams(); // Get the dynamic outlet name from the route
+
+  const navigate = useNavigate();
+
+  const [message, setMessage] = useState("");
+
+  const [formData, setFormData] = useState({
+    pledgeID: "",
+    username: "",
+    item: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/redeem", {
+        ...formData,
+        outlet,
+      });
+      if (response.data.success) {
+        // redirect to success page
+        navigate("/success", { message: response.data.message });
+      }
+    } catch (error) {
+      console.error("Error redeeming:", error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        setMessage(error.response.data.error);
+      } else if (error.request) {
+        // The request was made but no response was received
+        setMessage("No response received from the server");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        setMessage("An error occurred while setting up the request");
+      }
+    }
+  };
 
   return (
     <div className="outlet template d-flex justify-content-center align-items-center vh-100 bg-light">
@@ -14,28 +57,46 @@ function OutletPage() {
         <p>Built using Hyperledger Fabric</p>
       </div>
       <div className="form_container p-5 rounded bg-white ms-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h3 className="text-center">Redeem token</h3>
+
           <div className="mb-2">
-            <label htmlFor="Token_ID">Token ID:</label>
+            <label htmlFor="Pledge_ID">Pledge ID:</label>
             <input
               type="text"
-              name="Token_ID"
-              placeholder="Enter token ID"
+              name="Pledge_ID"
+              placeholder="Enter pledge ID"
+              onChange={handleChange}
+              defaultValue={formData.pledgeID}
               className="form-control"
             />
           </div>
+
           <div className="mb-2">
             <label htmlFor="username">Username:</label>
             <input
               type="text"
               name="username"
               placeholder="Enter your username"
+              onChange={handleChange}
+              defaultValue={formData.username}
               className="form-control"
             />
           </div>
 
-          <div className="d-grid">
+          <div className="mb-2">
+            <label htmlFor="item">Item:</label>
+            <input
+              type="text"
+              name="item"
+              placeholder="Enter item"
+              onChange={handleChange}
+              defaultValue={formData.item}
+              className="form-control"
+            />
+          </div>
+
+          <div className="d-grid" type="submit">
             <button type="redeem" className="btn btn-outline-success">
               Redeem
             </button>
