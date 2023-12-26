c6d110b368e502aee06abf004ea6505ef9045f9f
Nishtha Das <nishtha3@hotmail.com>
2023-12-27 02:40:08 +0530

    cleaning up node server file

--- backend/javascript/app.js
+++ backend/javascript/app.js
@@ -21,9 +21,6 @@ const app = express();
 app.use(cors());
 const PORT = 8080;
 
-// var jsonParser = bodyParser.json();
-// var textParser = app.use(bodyParser.text({ type: 'text/*' }));
-
 app.use(bodyParser.json());
 app.use(express.json());
 
@@ -162,30 +159,6 @@ app.post("/redeem", async (req, res) => {
         res.status(500).json({ success: false, error: error.message });
     }
 });
-
-/** 
-
-app.get('/users', textParser, async (req, res) => {
-    console.log("Entered get request.");
-    var username = req.body;
-    console.log(username);
-    res.send("true");
-});
-
-app.post('/users', textParser, async (req, res) => {
-    console.log("Entered post request.");
-    var username = req.body;
-    console.log(username);
-    try {
-        var userType = await getUserType(username);
-        console.log(userType);
-        res.json(userType);
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-    }
-});
-
-*/
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
